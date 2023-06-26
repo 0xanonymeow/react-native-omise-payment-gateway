8fb3c087223b07fcda7bfdbd514da0a2b2c1c6b7
0xanonymeow <anonymeow@incatni.to>
2023-06-27 03:41:33 +0700

    feat: display cards on main

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/native';
 import { useNavigation } from '@react-navigation/native';
+import { Card } from 'components/card';
 import { Container } from 'components/container';
 import { Heading, Subheading } from 'components/text';
 import { NavigationProps, ROUTES } from 'constants/navigation';
+import { map } from 'lodash';
 import { TouchableOpacity } from 'react-native';
+import { useCardStore } from 'store/useCardStore';
 import { Card as CardIcon } from '../../../assets/icons';
 
 const NoCardsContainer = styled.View`
@@ -16,9 +19,17 @@ const ButtonText = styled(Heading)`
   color: #4ad8da;
 `;
 
+const CardsContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 32px;
+`;
+
 const NoCards = () => {
   const { navigate } = useNavigation<NavigationProps>();
-  const onAdd = () => navigate(ROUTES.CARD);
+  const onAdd = () => navigate(ROUTES.ADD_CARD);
 
   return (
     <NoCardsContainer>
@@ -35,9 +46,23 @@ const NoCards = () => {
 };
 
 export const Main = () => {
+  const cards = useCardStore((state) => state.cards);
+
   return (
-    <Container contentContainerStyle={{ flex: 1 }}>
-      <NoCards />
+    <Container
+      contentContainerStyle={{
+        flex: cards.length ? 0 : 1,
+      }}
+    >
+      {cards.length ? (
+        <CardsContainer>
+          {map(cards, (card) => (
+            <Card key={card.number} {...card} />
+          ))}
+        </CardsContainer>
+      ) : (
+        <NoCards />
+      )}
     </Container>
   );
 };
