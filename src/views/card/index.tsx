0035a7983c1eba4de79df60e49388f2dfd3a4c7f
0xanonymeow <anonymeow@incatni.to>
2023-06-27 01:11:59 +0700

    feat: add card inputs

--- src/views/card/index.tsx
+++ src/views/card/index.tsx
@@ -1,5 +1,102 @@
-import { View } from 'react-native';
+import styled from '@emotion/native';
+import { Container } from 'components/container';
+import { TextInput } from 'components/input';
+import { useRef } from 'react';
+import {
+  TextInput as DefaultTextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ViewStyle,
+} from 'react-native';
+import {
+  JCB,
+  Mastercard,
+  MastercardSecurecode,
+  Omise,
+  VerifiedByVisa,
+  VisaH16,
+} from '../../../assets/icons';
+
+const Group = styled.View`
+  gap: 24px;
+  flex: 1;
+  // justify-content: space-around;
+`;
+
+const Row = styled.View<ViewStyle>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  ${(props) => props.gap && `gap: ${props.gap}px`}
+  ${(props) => props.width && `width: ${props.width}`};
+  ${(props) => props.alignSelf && `align-self: ${props.alignSelf}`}
+`;
+
+const CardNumberRightIcons = () => (
+  <>
+    <VisaH16 width={24} height={16} />
+    <Mastercard width={24} height={16} />
+    <JCB width={24} height={16} />
+  </>
+);
 
 export const Card = () => {
-  return <View></View>;
+  const numberRef = useRef<DefaultTextInput>(null);
+  const nameRef = useRef<DefaultTextInput>(null);
+  const expRef = useRef<DefaultTextInput>(null);
+  const cvvRef = useRef<DefaultTextInput>(null);
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={100}
+    >
+      <Container>
+        <Group>
+          <TextInput
+            ref={numberRef}
+            label="ATM/Debit/Credit card number"
+            placeholder="0000 0000 0000 0000"
+            returnKeyType="next"
+            onSubmitEditing={() => nameRef.current?.focus()}
+            RightIcons={CardNumberRightIcons()}
+          />
+          <TextInput
+            ref={nameRef}
+            label="Name on Card"
+            placeholder="John Doe"
+            returnKeyType="next"
+            onSubmitEditing={() => expRef.current?.focus()}
+          />
+          <Row gap={18}>
+            <TextInput
+              ref={expRef}
+              label="Expiry Date"
+              placeholder="MM/YY"
+              returnKeyType="next"
+              onSubmitEditing={() => cvvRef.current?.focus()}
+              containerProps={{
+                width: '47.5%',
+              }}
+            />
+            <TextInput
+              ref={cvvRef}
+              label="CVV"
+              returnKeyType="send"
+              containerProps={{
+                width: '47.5%',
+              }}
+            />
+          </Row>
+          <Row gap={20} width="50%" alignSelf="center" marginTop={40}>
+            <VerifiedByVisa />
+            <MastercardSecurecode />
+            <Omise />
+          </Row>
+        </Group>
+      </Container>
+    </KeyboardAvoidingView>
+  );
 };
