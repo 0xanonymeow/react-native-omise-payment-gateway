80b761a7a6ca1ba8c4a90102eeacf4f52b9a0ddd
0xanonymeow <anonymeow@incatni.to>
2023-06-26 21:24:57 +0700

    feat: add fonts, icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { Main } from 'views/main';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Navigation } from 'navigation';
+import { useCallback } from 'react';
 import { Layout } from './Layout';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    'FC-Subject-Rounded-Bold': require('./assets/fonts/FC-Subject-Rounded-Bold.ttf'),
+    'FC-Subject-Rounded-Regular': require('./assets/fonts/FC-Subject-Rounded-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Layout>
-      <Main />
+    <Layout onLayout={onLayoutRootView}>
+      <Navigation />
     </Layout>
   );
 };
---
+++ assets/icons/index.tsx
@@ -0,0 +1,21 @@
+import Card from './card.svg';
+import JCB from './jcb-color.svg';
+import LeftArrow from './left-arrow.svg';
+import Mastercard from './mastercard-color.svg';
+import MastercardSecurecode from './mastercard-securecode-grey.svg';
+import Omise from './omise-grey.svg';
+import Plus from './plus.svg';
+import VerifiedByVisa from './verified-by-visa.svg';
+import VisaH16 from './visa-h16-color.svg';
+
+export {
+  Card,
+  JCB,
+  LeftArrow,
+  Mastercard,
+  MastercardSecurecode,
+  Omise,
+  Plus,
+  VerifiedByVisa,
+  VisaH16,
+};
